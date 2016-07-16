3d171564d99ce4ebe88b0723e94d73f6f5ef883e
Mark Dietzer <mriq91@gmail.com>
2016-07-16 19:30:49 +0200

    Now it works. Locally. Travis doesn't work

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -e
-export MAVEN_ARGUMENTS="$1"
-# -Dfile=X -DpomFile=Y
+#!/bin/bash
+set -e
+
+export MAVEN_ARGUMENTS="-DaltDeploymentRepository=deployment-repo-id::default::https://maven.doridian.net/repository/maven-snapshots/ -DrepositoryId=deployment-repo-id -Durl=https://maven.doridian.net/repository/maven-snapshots/"
+
 bash spigot-build.sh
--- spigot-build.sh
+++ spigot-build.sh
@@ -1,7 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
+
 mkdir -p Spigot
 cd Spigot
+
 wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 java -jar BuildTools.jar
+
 cd Spigot
 mvn deploy $MAVEN_ARGUMENTS
